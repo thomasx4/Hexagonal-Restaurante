210ccff18f1924cfab6595d722ce4af15821cee1
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-03-19 18:51:19 -0500

    M - feat: I'm adding the file with its functionalities 'CategoryInputPort.java' input

--- src/main/java/com/restaurante/hexagonal/application/ports/input/CategoryInputPort.java
+++ src/main/java/com/restaurante/hexagonal/application/ports/input/CategoryInputPort.java
@@ -1,5 +1,14 @@
 package com.restaurante.hexagonal.application.ports.input;
 
-public class CategoryInputPort {
-    
+import java.util.List;
+
+import com.restaurante.hexagonal.domain.model.Category;
+
+public interface CategoryInputPort {
+ Category createCategory(String name, String description);
+    List<Category> getAllCategories();
+    Category getCategoryById(Long id);
+    Category updCategory(Long id, String name, String description);
+    void deleteCategory(Long id);
+    List<Category> searchCategories(String searchTerm);  
 }
