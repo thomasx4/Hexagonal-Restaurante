8a8e4d1b9b48ba7c67c839897f03f4ede54addb9
thomasx4 <castanedamunoz23@gmail.com>
2026-03-20 07:27:47 -0500

    TD - feat: PaymentEntity.java Entity

--- src/main/java/com/restaurante/hexagonal/infrastructure/persistence/entity/PaymentEntity.java
+++ src/main/java/com/restaurante/hexagonal/infrastructure/persistence/entity/PaymentEntity.java
@@ -1,5 +1,30 @@
 package com.restaurante.hexagonal.infrastructure.persistence.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "payments")
 public class PaymentEntity {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "order_id")
+    private Long orderId;
+
+    private BigDecimal amount;
+
+    @Column(name = "payment_method")
+    private String paymentMethod;
+
+    @Column(name = "payment_date")
+    private LocalDateTime paymentDate;
+
+    private String reference;
 }
