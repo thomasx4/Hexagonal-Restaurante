ecf9d0aa12eed7c2e4b12b55a2816a6e9a294b5b
thomasx4 <castanedamunoz23@gmail.com>
2026-03-19 22:31:40 -0500

    TD - feat: JpaOrderDetailRepository.java Repository

--- src/main/java/com/restaurante/hexagonal/infrastructure/persistence/repository/JpaOrderDetailRepository.java
+++ src/main/java/com/restaurante/hexagonal/infrastructure/persistence/repository/JpaOrderDetailRepository.java
@@ -1,5 +1,10 @@
 package com.restaurante.hexagonal.infrastructure.persistence.repository;
 
-public class JpaOrderDetailRepository {
+import com.restaurante.hexagonal.infrastructure.persistence.entity.OrderDetailEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JpaOrderDetailRepository extends JpaRepository<OrderDetailEntity, Long> {
     
 }
