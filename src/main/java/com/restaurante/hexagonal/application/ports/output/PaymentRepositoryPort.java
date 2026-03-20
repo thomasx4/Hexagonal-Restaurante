04f2d5d58f8f725c50d90cacff97b84fc7409bb4
thomasx4 <castanedamunoz23@gmail.com>
2026-03-20 07:35:56 -0500

    TD - feat: PaymentRepositoryPort.java output

--- src/main/java/com/restaurante/hexagonal/application/ports/output/PaymentRepositoryPort.java
+++ src/main/java/com/restaurante/hexagonal/application/ports/output/PaymentRepositoryPort.java
@@ -1,5 +1,19 @@
 package com.restaurante.hexagonal.application.ports.output;
 
-public class PaymentRepositoryPort {
-    
+import com.restaurante.hexagonal.domain.model.Payment;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PaymentRepositoryPort {
+
+    Payment save(Payment payment);
+
+    Optional<Payment> findById(Long id);
+
+    List<Payment> findAll();
+
+    Payment update(Payment payment);
+
+    void deleteById(Long id);
 }
