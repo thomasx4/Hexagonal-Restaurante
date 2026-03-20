8a9f2818b492d87af7f34ba8b802ad5c40edfad1
thomasx4 <castanedamunoz23@gmail.com>
2026-03-19 22:03:26 -0500

    TD - feat: OrderDetailPersistenceAdapter.java Adapter

--- src/main/java/com/restaurante/hexagonal/infrastructure/persistence/adapter/OrderDetailPersistenceAdapter.java
+++ src/main/java/com/restaurante/hexagonal/infrastructure/persistence/adapter/OrderDetailPersistenceAdapter.java
@@ -1,5 +1,52 @@
 package com.restaurante.hexagonal.infrastructure.persistence.adapter;
 
-public class OrderDetailPersistenceAdapter {
-    
+import com.restaurante.hexagonal.application.ports.output.OrderDetailRepositoryPort;
+import com.restaurante.hexagonal.domain.model.OrderDetail;
+import com.restaurante.hexagonal.infrastructure.persistence.entity.OrderDetailEntity;
+import com.restaurante.hexagonal.infrastructure.persistence.mapper.OrderDetailMapper;
+import com.restaurante.hexagonal.infrastructure.persistence.repository.JpaOrderDetailRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class OrderDetailPersistenceAdapter implements OrderDetailRepositoryPort {
+
+    private final JpaOrderDetailRepository repository;
+    private final OrderDetailMapper mapper;
+
+    @Override
+    public OrderDetail save(OrderDetail orderDetail) {
+        OrderDetailEntity entity = mapper.toEntity(orderDetail);
+        return mapper.toDomain(repository.save(entity));
+    }
+
+    @Override
+    public Optional<OrderDetail> findById(Long id) {
+        return repository.findById(id)
+                .map(mapper::toDomain);
+    }
+
+    @Override
+    public List<OrderDetail> findAll() {
+        return repository.findAll()
+                .stream()
+                .map(mapper::toDomain)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public OrderDetail update(OrderDetail orderDetail) {
+        OrderDetailEntity entity = mapper.toEntity(orderDetail);
+        return mapper.toDomain(repository.save(entity));
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
 }
