f9ddb07280a79a4034059071245cc6712d361c5a
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-03-19 19:00:01 -0500

    M - feat: Add file with its functionalities 'CategoryRepositoryPort.java' output

--- src/main/java/com/restaurante/hexagonal/application/ports/output/CategoryRepositoryPort.java
+++ src/main/java/com/restaurante/hexagonal/application/ports/output/CategoryRepositoryPort.java
@@ -1,5 +1,15 @@
 package com.restaurante.hexagonal.application.ports.output;
 
-public class CategoryRepositoryPort {
-    
+import java.util.List;
+import java.util.Optional;
+
+import com.restaurante.hexagonal.domain.model.Category;
+
+public interface CategoryRepositoryPort {
+    Category save (Category category);
+    Optional<Category> findById (Long id);
+    List<Category> findAll();
+    List<Category> findByNameContaining(String searchTerm);
+    boolean existsByName (String name);
+    void deleteById(Long id);
 }
