ec311cc10a6a05324f9d65c07539bed2c81ae1fb
thomasx4 <castanedamunoz23@gmail.com>
2026-03-20 07:41:25 -0500

    TD - feat: JpaPaymentRepository.java repository

--- src/main/java/com/restaurante/hexagonal/infrastructure/persistence/repository/JpaPaymentRepository.java
+++ src/main/java/com/restaurante/hexagonal/infrastructure/persistence/repository/JpaPaymentRepository.java
@@ -1,5 +1,9 @@
 package com.restaurante.hexagonal.infrastructure.persistence.repository;
 
-public class JpaPaymentRepository {
-    
+import com.restaurante.hexagonal.infrastructure.persistence.entity.PaymentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JpaPaymentRepository extends JpaRepository<PaymentEntity, Long> {
 }
