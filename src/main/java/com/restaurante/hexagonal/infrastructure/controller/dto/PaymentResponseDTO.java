2cf0a71dc020db95fbbe702f8236ea47b571ac57
thomasx4 <castanedamunoz23@gmail.com>
2026-03-20 07:52:34 -0500

    TD - feat: PaymentResponseDTO.java DTO

--- src/main/java/com/restaurante/hexagonal/infrastructure/controller/dto/PaymentResponseDTO.java
+++ src/main/java/com/restaurante/hexagonal/infrastructure/controller/dto/PaymentResponseDTO.java
@@ -1,5 +1,17 @@
 package com.restaurante.hexagonal.infrastructure.controller.dto;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
 public class PaymentResponseDTO {
-    
+
+    private Long id;
+    private Long orderId;
+    private BigDecimal amount;
+    private String paymentMethod;
+    private LocalDateTime paymentDate;
+    private String reference;
 }
