bc2d71d31a3d74a01efa6be66e53685dcf9ebea0
thomasx4 <castanedamunoz23@gmail.com>
2026-03-19 22:25:36 -0500

    TD - feat: OrderDetailResponseDTO.java DTO

--- src/main/java/com/restaurante/hexagonal/infrastructure/controller/dto/OrderDetailResponseDTO.java
+++ src/main/java/com/restaurante/hexagonal/infrastructure/controller/dto/OrderDetailResponseDTO.java
@@ -1,5 +1,16 @@
 package com.restaurante.hexagonal.infrastructure.controller.dto;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class OrderDetailResponseDTO {
-    
+
+    private Long id;
+    private Long orderId;
+    private Long productId;
+    private Integer quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal subtotal;
 }
